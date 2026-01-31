5724f6063f5cbba30eacd277a9df93fed2ba3e3c
Farhan Khan <shaikhfarhank94@gmail.com>
2026-01-31 15:39:18 +0530

    Remove onClick from Get Started button
    Removed onClick handler from 'Get Started' button.

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
       </h1>
       <h2 className="text-3xl sm:text-5xl  font-semibold mb-4">Welcome to our app</h2>
       <p className="mb-8 max-w-md">Let's start with a quick product  tour and we will have you up and running in no time. </p>
-      <button className="border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-200 transition-all" onClick={()=>navigate('/login')}>Get Started</button>
+      <button className="border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-200 transition-all">Get Started</button>
     </div>
   );
 };
